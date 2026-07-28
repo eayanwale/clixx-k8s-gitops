57d48e7736dd1fe9ba4aa1b8bf3fd3c36a98eae2
Enoch Ayanwale <enochayanwale@outlook.com>
2026-07-28 17:16:26 -0400

    Add custom-domain Ingress, ALB-forwarded-proto handling in wp-config.php

--- manifests/wp-config.php
+++ manifests/wp-config.php
@@ -1,4 +1,9 @@
 <?php
+
+if ( isset( $_SERVER['HTTP_X_FORWARDED_PROTO'] ) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' ) {
+    $_SERVER['HTTPS'] = 'on';
+}
+
 /**
  * The base configuration for WordPress
  *
